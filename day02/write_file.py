7d888fd17b9488220cde72ea5d0315b4d9af4414
Minjae Jin <moc0etan@gmail.com>
2018-12-19 12:30:40 +0900

    commit

---
+++ day02/write_file.py
@@ -0,0 +1,9 @@
+# f=open('ssafy.txt', 'w') # w: write, r: read, a: append
+# f.write('This is SSAFY!')
+# f.close()
+
+with open('ssafy.txt', 'w', encoding='utf8') as f:
+    f.writelines(['1\n', '2\n', '3\n'])
+    #for i in range(10):
+    #   f.write(f'This is \'SSAFY\'! {i}\n')
+    #   # \t : tab, \\: '\' 문자, \' & \" : 따옴표, 쌍따옴표 문자
