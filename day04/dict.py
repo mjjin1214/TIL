f5dfd05ef01d6611d37d91c0b8cdece4e2190a86
Minjae Jin <moc0etan@gmail.com>
2018-12-20 09:37:23 +0900

    commit

--- day04/dict.py
+++ day04/dict.py
@@ -2,4 +2,21 @@ lunch = {
     '중국집':'02-123-123',
     '양식집':'054-123-123',
 }
-print(lunch)
+
+dinner = dict(중국집='02-123-123')
+
+lunch['분식집'] = '053-123-123'
+
+print(lunch['중국집'])
+
+for key in lunch:
+    print(key)
+    print(lunch[key])
+
+idol = {
+    'bts': {
+        '지민':24,
+        'RM':25
+    }
+}
+idol['bts']['RM']
