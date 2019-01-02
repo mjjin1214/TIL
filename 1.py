65a4e9261629a27e5438052a97465ceeec5cfefc
Minjae Jin <moc0etan@gmail.com>
2019-01-02 17:56:00 +0900

    D01 | 190102 PM | Python intro & workshop

--- 1.py
+++ 1.py
@@ -1,14 +1,8 @@
-A = int(input())
-n = A//5
-count = 0
-for i in range(n):
-    if (A-(5*(n-i)))%3 == 0:
-        print((n-i)+(A-(5*(n-i)))//3)
-        count += 1
-        break
-    elif A%3 == 0:
-        print(A//3)
-        count += 1
-        break
-if count == 0:
-    print(-1)
+import sys
+a = list(range(1, int(sys.stdin.readline())+1))
+for i in a:
+    b = list(map(int, list(str(i))))
+    if len(b) >= 3:
+        if b[0]-b[1] != b[1]-b[2]:
+            a.remove(a[i-1])
+print(len(a))
