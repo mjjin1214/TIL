15e0ee62c229bd24c261eb37e95f94fc4217605b
Minjae Jin <moc0etan@gmail.com>
2019-01-03 12:26:31 +0900

    D02 | 190103 |

--- 1.py
+++ 1.py
@@ -1,8 +1,6 @@
-import sys
-a = list(range(1, int(sys.stdin.readline())+1))
-for i in a:
-    b = list(map(int, list(str(i))))
-    if len(b) >= 3:
-        if b[0]-b[1] != b[1]-b[2]:
-            a.remove(a[i-1])
-print(len(a))
+print("""   
+   
+      """, end='')
+print("""  *
+ * *
+*****""")
