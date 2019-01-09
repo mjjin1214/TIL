466d669bfc152f24d1413196acf6d5c1b1cef1fa
Minjae Jin <moc0etan@gmail.com>
2019-01-09 17:56:36 +0900

    D06 | 190109 PM | Data structure (Dict, Set)

--- 1.py
+++ 1.py
@@ -1,13 +1,5 @@
-t = int(input())
-c = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D']
-for l in range(t):
-    n, k = map(int, input().split())
-    b = []
-    d = {}
-    for i in range(n):
-        a = list(map(int, input().split()))
-        b.append(a[0]*0.35 + a[1]*0.45 + a[2]*0.2)
-    e = sorted(b)
-    for j in range(len(e)):
-        d[e[j]] = c[9-int(j//(n/10))]
-    print(f'#{l+1} {d[b[k-1]]}')
+def my_mul(x=1, y=1):
+	return x*y
+
+print(my_mul())
+print(my_mul(3,5))
---
+++ Python/module_test/b.py
@@ -0,0 +1,7 @@
+import a
+from my_math import add, mul
+import random
+
+
+print(add(2,3))
+print(mul(2,3))
