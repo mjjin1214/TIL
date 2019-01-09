e77209ff32a9c78bfedce619cf2e017d41631009
Minjae Jin <moc0etan@gmail.com>
2019-01-09 12:23:19 +0900

    D06 | 190109 AM | Problems

--- 1.py
+++ 1.py
@@ -1,6 +1,13 @@
-a = int(input())
-for i in range(a):
-    b = 0
-    for j in range(int(input())):
-        b = b + ((-1)**(j))*(j+1)
-    print(f'#{i+1} {b}')
+t = int(input())
+c = ['A+', 'A0', 'A-', 'B+', 'B0', 'B-', 'C+', 'C0', 'C-', 'D']
+for l in range(t):
+    n, k = map(int, input().split())
+    b = []
+    d = {}
+    for i in range(n):
+        a = list(map(int, input().split()))
+        b.append(a[0]*0.35 + a[1]*0.45 + a[2]*0.2)
+    e = sorted(b)
+    for j in range(len(e)):
+        d[e[j]] = c[9-int(j//(n/10))]
+    print(f'#{l+1} {d[b[k-1]]}')
