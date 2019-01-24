017e20f18f37ee216c8b7df3491804ca3c6a5889
Minjae Jin <moc0etan@gmail.com>
2019-01-24 17:53:03 +0900

    D17 | 190124 PM | Django

--- 1.py
+++ 1.py
@@ -1,10 +1,5 @@
-a = {
-    "TV": 2000000,
-    "냉장고": 1500000,
-    "책상": 350000,
-    "노트북": 1200000,
-    "가스레인지": 200000,
-    "세탁기": 1000000
-}
-for i in a.items():
-    print(i)
+class Korean():
+    @staticmethod
+    def printNationality():
+        print('대한민국')
+Korean.printNationality()
